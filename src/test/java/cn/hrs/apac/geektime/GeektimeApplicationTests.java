c45a46a2f42a3b173a428f7002bec7988e34569e
abe07 <abe07@mo-cn-413int.hrs.com>
2019-12-31 17:22:27 +0800

    Finished in Part 2, 06 in total

---
+++ src/test/java/cn/hrs/apac/geektime/GeektimeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hrs.apac.geektime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeektimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
